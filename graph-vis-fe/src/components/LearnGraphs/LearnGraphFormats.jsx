6731f2a5e4943f90c6c739ccf7ca3102d2bb9aab
Michal-Madejski <m.madejski@perfectone.pl>
2024-07-03 01:39:47 +0200

    adjacency and list synced

--- graph-vis-fe/src/components/LearnGraphs/LearnGraphFormats.jsx
+++ graph-vis-fe/src/components/LearnGraphs/LearnGraphFormats.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/learnGraph.styles.scss";
 
 const LearnGraphFormats = ({ graphFormats }) => {
+  const [hoveredNode, setHoveredNode] = useState(null);
+  console.log("🚀 ~ LearnGraphFormats ~ hoveredNode:", hoveredNode);
   const matrixSize = graphFormats.adjacency_matrix
     ? graphFormats.adjacency_matrix.length
     : 0;
+
+  const handleMouseEnter = (node) => {
+    setHoveredNode(node.toString());
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredNode(null);
+  };
+
+  const isNodeHovered = (node) => {
+    return node.toString() === hoveredNode;
+  };
+
+  const highlightLine = (line, node) => {
+    return node !== null && new RegExp(`\\b${node}\\b`).test(line);
+  };
+
   return (
     <div className="graph-formats">
       <h3>Graph Formats</h3>
@@ -14,8 +33,13 @@ const LearnGraphFormats = ({ graphFormats }) => {
           {graphFormats.adjacency_list &&
             Object.entries(graphFormats.adjacency_list).map(
               ([node, edges], index) => (
-                <div key={index} className="grid-item">
-                  <strong>{node}:</strong> {edges.join(", ")}
+                <div
+                  key={index}
+                  className={`grid-item ${isNodeHovered(node) ? "hovered" : ""}`}
+                  onMouseEnter={() => handleMouseEnter(node)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <strong>{Number(node)}:</strong> {edges.join(", ")}
                 </div>
               )
             )}
@@ -26,8 +50,7 @@ const LearnGraphFormats = ({ graphFormats }) => {
         <div className="matrix-container">
           {/* Heading Row */}
           <div className="matrix-row">
-            <div className="matrix-header-cell"></div>{" "}
-            {/* Empty cell for alignment */}
+            <div className="matrix-header-cell"></div>
             {Array.from({ length: matrixSize }, (_, index) => (
               <div key={index} className="matrix-header-cell">
                 <div className="matrix-head-cell-container">{index}</div>
@@ -36,10 +59,15 @@ const LearnGraphFormats = ({ graphFormats }) => {
           </div>
           {graphFormats.adjacency_matrix &&
             graphFormats.adjacency_matrix.map((row, rowIndex) => (
-              <div key={rowIndex} className="matrix-row">
+              <div
+                key={rowIndex}
+                className={`matrix-row ${isNodeHovered(rowIndex) ? "hovered" : ""}`}
+                onMouseEnter={() => handleMouseEnter(rowIndex)}
+                onMouseLeave={handleMouseLeave}
+              >
                 <div className="matrix-row-id">{rowIndex}</div>
                 {row.map((cell, cellIndex) => (
-                  <div key={cellIndex} className="matrix-cell">
+                  <div key={cellIndex} className={`matrix-cell`}>
                     {cell}
                   </div>
                 ))}
@@ -49,15 +77,39 @@ const LearnGraphFormats = ({ graphFormats }) => {
       </div>
       <div className="format-section">
         <h4>DOT Format</h4>
-        <pre>{graphFormats.dot}</pre>
+        <pre
+          className={
+            highlightLine(graphFormats.dot, hoveredNode) ? "hovered" : ""
+          }
+          onMouseEnter={() => handleMouseEnter(hoveredNode)}
+          onMouseLeave={handleMouseLeave}
+        >
+          {graphFormats.dot}
+        </pre>
       </div>
       <div className="format-section">
         <h4>GML Format</h4>
-        <pre>{graphFormats.gml}</pre>
+        <pre
+          className={
+            highlightLine(graphFormats.gml, hoveredNode) ? "hovered" : ""
+          }
+          onMouseEnter={() => handleMouseEnter(hoveredNode)}
+          onMouseLeave={handleMouseLeave}
+        >
+          {graphFormats.gml}
+        </pre>
       </div>
       <div className="format-section">
         <h4>GraphML Format</h4>
-        <pre>{graphFormats.graphml}</pre>
+        <pre
+          className={
+            highlightLine(graphFormats.graphml, hoveredNode) ? "hovered" : ""
+          }
+          onMouseEnter={() => handleMouseEnter(hoveredNode)}
+          onMouseLeave={handleMouseLeave}
+        >
+          {graphFormats.graphml}
+        </pre>
       </div>
     </div>
   );
