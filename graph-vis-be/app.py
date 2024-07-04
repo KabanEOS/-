d48643898dda8d665f5470a7f90acd277c62f917
Michal-Madejski <m.madejski@perfectone.pl>
2024-07-04 17:11:53 +0200

    preparing for heroku deploy

--- graph-vis-be/app.py
+++ graph-vis-be/app.py
@@ -101,5 +101,4 @@ def get_traversal():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, port=5001)
